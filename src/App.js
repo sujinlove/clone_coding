4c5c71686f84b55cd7bffbb4dad1ea709268788e
soojin-enius <sujinlove9491@gmail.com>
2020-10-03 16:14:24 +0900

    Add loading & install axios

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
-  componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("I just upated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  }
-
   state = {
-    count: 0
+    isLoading: true,
+    movie: []
   };
-
-  add = () => {
-    this.setState(current => ({
-      // current에 현재 state가 넘어온다.
-      count: current.count + 1 // 그 state의 count에 1을 더한다.
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1
-    }));
-  };
-
+  componentDidMount() {
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>This number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
