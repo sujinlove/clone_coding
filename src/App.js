b106190293e377a5632f952578737354e51b5d37
soojin-enius <sujinlove9491@gmail.com>
2020-10-03 16:51:59 +0900

    get Movie data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
   };
+
+  getMoives = async () => {
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // this.setState({ movies: movies });
+    //console.log(movies);
+    this.setState({ movies, isLoading: false }); // ES6에서는 객체의 키와 대입할 변수의 이름이 같으면, 코드를 축약할 수 있다.
+  };
+
   componentDidMount() {
     // 영화 데이터 로딩!
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMoives();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
