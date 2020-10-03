b96986a3d0ccf98c00998eed113bec026ea9b8ec
soojin-enius <sujinlove9491@gmail.com>
2020-10-03 18:01:33 +0900

    Add array genres of movie data

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="movie__genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
@@ -19,7 +28,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
